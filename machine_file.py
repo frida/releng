9e818c2ec2fdb63debd92a2b23b200a272cbaa16
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-04-03 12:00:27 +0200

    machine_file: Remove unused to-env code
    Only needed by the legacy deps build system.

--- machine_file.py
+++ machine_file.py
@@ -1,68 +1,6 @@
-#!/usr/bin/env python3
-
-import argparse
 from configparser import ConfigParser
 from pathlib import Path
-import sys
 from typing import Dict, List, Sequence, Union
-
-
-class CommandError(Exception):
-    pass
-
-
-def main():
-    parser = argparse.ArgumentParser(description="Tool for working with Meson machine files.")
-    subparsers = parser.add_subparsers()
-
-    command = subparsers.add_parser("to-env", help="generate a legacy build environment from a machine file")
-    command.add_argument("machine_file", help="machine file")
-    command.add_argument("--flavor", help="environment flavor", default="c", choices=["c", "cpp"])
-    command.set_defaults(func=lambda args: to_env(Path(args.machine_file), args.flavor))
-
-    args = parser.parse_args()
-    if 'func' in args:
-        try:
-            args.func(args)
-        except CommandError as e:
-            print(e, file=sys.stderr)
-            sys.exit(1)
-    else:
-        parser.print_usage(file=sys.stderr)
-        sys.exit(1)
-
-
-def to_env(mfile: Path, flavor: Union["c", "cpp"]):
-    config = load(mfile)
-
-    if flavor == "c":
-        entries = [
-            ("CC", "c"),
-            ("CFLAGS", "c_args"),
-            ("LDFLAGS", "c_link_args"),
-        ]
-    else:
-        entries = [
-            ("CXX", "cpp"),
-            ("CXXFLAGS", "cpp_args"),
-            ("LDFLAGS", "cpp_link_args"),
-        ]
-    entries += [
-        ("AR", "ar"),
-        ("NM", "nm"),
-        ("RANLIB", "ranlib"),
-        ("STRIP", "strip"),
-        ("READELF", "readelf"),
-        ("OBJCOPY", "objcopy"),
-        ("OBJDUMP", "objdump"),
-        ("PKG_CONFIG", "pkgconfig"),
-    ]
-    for envvar_name, property_name in entries:
-        value = config.get(property_name, None)
-        if value is None:
-            continue
-        encoded_value = " ".join(value)
-        print(f"export {envvar_name}=\"{encoded_value}\"")
 
 
 def load(mfile: Path) -> Dict[str, Union[str, List[str]]]:
@@ -104,7 +42,3 @@ def strv_to_meson(strv: Sequence[str]) -> str:
 
 def str_to_meson(s: str) -> str:
     return "'" + s + "'"
-
-
-if __name__ == "__main__":
-    main()
