0107acaf0bda3889363faf69e68a6cfcb6ec559a
WorksButNotTested <62701594+WorksButNotTested@users.noreply.github.com>
2025-03-12 07:57:51 +0000

    Add support for arm64be (#9)

--- machine_spec.py
+++ machine_spec.py
@@ -63,6 +63,8 @@ class MachineSpec:
                         arch = "armeabi"
                 elif arch == "aarch64":
                     arch = "arm64"
+                elif arch == "aarch64_be":
+                    arch = "arm64be"
 
                 if system.startswith("musl"):
                     config = "musl"
@@ -257,6 +259,7 @@ CPU_FAMILIES = {
     "armhf":      "arm",
 
     "arm64":      "aarch64",
+    "arm64be":    "aarch64",
     "arm64e":     "aarch64",
     "arm64eoabi": "aarch64",
 
@@ -272,6 +275,7 @@ CPU_TYPES = {
     "armhf":      "armv7hf",
     "armeabi":    "armv7eabi",
 
+    "aarch64_be": "aarch64",
     "arm64":      "aarch64",
     "arm64e":     "aarch64",
     "arm64eoabi": "aarch64",
@@ -299,6 +303,7 @@ CPU_TYPES_PER_OS_OVERRIDES = {
 }
 
 BIG_ENDIAN_ARCHS = {
+    "arm64be",
     "armbe8",
     "mips",
     "mips64",
@@ -307,4 +312,4 @@ BIG_ENDIAN_ARCHS = {
     "s390x",
 }
 
-TARGET_TRIPLET_ARCH_PATTERN = re.compile(r"^(i.86|x86_64|arm\w*|aarch64|mips\w*|powerpc|s390x)$")
+TARGET_TRIPLET_ARCH_PATTERN = re.compile(r"^(i.86|x86_64|arm\w*|aarch64(_be)?|mips\w*|powerpc|s390x)$")
